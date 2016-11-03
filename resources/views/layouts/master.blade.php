f2074f52e05d111222410ea665d0fd8db71c40a0
Jeannette DuVal <duval@g.harvard.edu>
2016-11-03 02:51:52 -0400

    minor design repair

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,7 +23,8 @@
 
     <header class=bgimage>
       <div class="container">
-        <h1><a href="{{ route('HomePage') }}">@yield('title','Developer\'s Best Friend')</a></h1>
+        <!-- <h1><a href="{{ route('HomePage') }}">@yield('title','Developer\'s Best Friend')</a></h1> -->
+        <h1>@yield('title','Developer\'s Best Friend')</h1>
         by Jeannette D.
       </div>
     </header>
